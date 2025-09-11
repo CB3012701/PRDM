434b4c4d176e7c2b005aa3e6fff6dbda5e89eb70
Érica Barbosa <82848078+CB3012701@users.noreply.github.com>
2025-09-11 17:54:03 -0300

    Create AppShell.xaml.cs

---
+++ TP01/PRDM_TP01/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+// Érica Barbosa Pereira Lobo CB3012701
+namespace CBTPRDM_TP01
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
